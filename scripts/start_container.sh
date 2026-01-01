3bb901a85230d78758e5b46a63abd20882e43f93
Varun Jaiswal <varjaisw.com>
2026-01-01 13:49:59 +0530

    added appspec file

---
+++ scripts/start_container.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set +e
+
+AWS_REGION=ap-south-1
+AWS_ACCOUNT_ID=123456789012
+IMAGE_NAME=image-share-backend
+
+aws ecr get-login-password --region $AWS_REGION \
+| docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:latest
+
+docker run -d \
+  --name image-share-backend \
+  -p 8080:8080 \
+  --restart always \
+  $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:latest
+echo "Container 'image-share-backend' has been started."
